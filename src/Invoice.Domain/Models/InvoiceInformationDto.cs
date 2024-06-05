00228cc253d12d0c01c22f2c872d3ef29e777e13
HasiBul Islam ShanTo <bdshanto@hotmail.com>
2024-06-06 00:29:28 +0600

    .net framework RDLC reporting

--- src/Invoice.Domain/Models/InvoiceInformationDto.cs
+++ src/Invoice.Domain/Models/InvoiceInformationDto.cs
@@ -1,12 +1,16 @@
-﻿namespace Invoice.Domain.Models;
+﻿using System;
+using System.Collections.Generic;
 
-public class InvoiceInformationDto
+namespace Invoice.Domain.Models
 {
-    public int Id { get; set; }
-    public string CustomerName { get; set; } = "";
-    public string Address { get; set; } = "";
-    public string InvoiceNo { get; set; } = "";
-    public DateTime OperationDate { get; set; }
-    public ICollection<InvoiceIteDto> InvoiceList { get; set; } = new List<InvoiceIteDto>();
+    public class InvoiceInformationDto
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string InvoiceNo { get; set; } = "";
+        public DateTime OperationDate { get; set; }
+        public ICollection<InvoiceIteDto> InvoiceList { get; set; } = new List<InvoiceIteDto>();
 
+    }
 }
