1f4ac051fc43c86beb6b25567fad14f0c67c581d
HasiBul Islam ShanTo <bdshanto@hotmail.com>
2024-06-05 23:35:54 +0600

    invoice added

---
+++ src/Invoice.Domain/Models/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice.Domain.Models
+{
+    public class InvoiceIteDto
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; } = "";
+        public decimal Qty { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
---
+++ src/Invoice.Domain/Models/InvoiceInformationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Invoice.Domain.Models;
+
+public class InvoiceInformationDto
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; } = "";
+    public string Address { get; set; } = "";
+    public string InvoiceNo { get; set; } = "";
+    public DateTime OperationDate { get; set; }
+    public ICollection<InvoiceIteDto> InvoiceList { get; set; } = new List<InvoiceIteDto>();
+
+}
